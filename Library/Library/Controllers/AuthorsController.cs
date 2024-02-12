5677ade81952dcb3cab23879a88a9e0b22081abf
Sharafiddin.Mukhtorov <129146459+SharafiddinMukhtorov@users.noreply.github.com>
2024-02-12 11:23:30 +0500

    Initial_Create

---
+++ Library/Library/Controllers/AuthorsController.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Library.DAL;
+using Library.Models;
+
+namespace Library.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private readonly LibraryDbContext _context;
+
+        public AuthorsController(LibraryDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Authors
+        public async Task<IActionResult> Index(string sortOrder, string? searchString)
+        {
+            ViewData["CurrentSort"] = sortOrder;
+            ViewData["FirstNameSort"] = sortOrder == "firstName_asc" ? "firstName_desc" : "firstName_asc";
+            ViewData["LastNameSort"] = sortOrder == "lastName_asc" ? "lastName_desc" : "lastName_asc";
+            ViewData["AgeSort"] = sortOrder == "age_asc" ? "age_desc" : "age_asc";
+
+            var authors = _context.Authors.AsQueryable();
+
+            if(searchString != null)
+            {
+                authors = authors
+                .Where(s => s.FirstName.ToLower().Contains(searchString.ToLower()))
+                .AsQueryable();
+            }
+
+            authors = sortOrder switch
+            {
+                "firstName_asc" => authors.OrderBy(x => x.FirstName),
+                "firstName_desc" => authors.OrderByDescending(x => x.FirstName),
+                "lastName_asc" => authors.OrderBy(x => x.LastName),
+                "lastName_desc" => authors.OrderByDescending(x => x.LastName),
+                "age_asc" => authors.OrderBy(x => x.Age),
+                "age_desc" => authors.OrderByDescending(x => x.Age),
+                _ => authors.OrderBy(x => x.FirstName)
+            };
+
+            return View(authors);
+        }
+
+        // GET: Authors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Authors == null)
+            {
+                return NotFound();
+            }
+
+            var author = await _context.Authors
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return View(author);
+        }
+
+        // GET: Authors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Authors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Age,Email,PhoneNumber")] Author author)
+        {
+            if (author != null)
+            {
+                _context.Add(author);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(author);
+        }
+
+        // GET: Authors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Authors == null)
+            {
+                return NotFound();
+            }
+
+            var author = await _context.Authors.FindAsync(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+            return View(author);
+        }
+
+        // POST: Authors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Age,Email,PhoneNumber")] Author author)
+        {
+            if (id != author.Id)
+            {
+                return NotFound();
+            }
+
+            if (author != null)
+            {
+                try
+                {
+                    _context.Update(author);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AuthorExists(author.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(author);
+        }
+
+        // GET: Authors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Authors == null)
+            {
+                return NotFound();
+            }
+
+            var author = await _context.Authors
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return View(author);
+        }
+
+        // POST: Authors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Authors == null)
+            {
+                return Problem("Entity set 'LibraryDbContext.Authors'  is null.");
+            }
+            var author = await _context.Authors.FindAsync(id);
+            if (author != null)
+            {
+                _context.Authors.Remove(author);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AuthorExists(int id)
+        {
+          return (_context.Authors?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
